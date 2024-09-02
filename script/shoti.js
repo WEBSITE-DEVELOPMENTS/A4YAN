ddada8706608ff985f3153d2c121ade55bbe8208
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-09-03 07:38:56 +0800

    Update shoti.js

--- script/shoti.js
+++ script/shoti.js
@@ -6,26 +6,24 @@ module.exports.config = {
   hasPermssion: 0,
   commandCategory: "other",
   usage: "[shoti]",
-  cooldowns: 5,
+  cooldowns: 60,
   dependencies: [],
-  usePrefix: true
+  usePrefix: true,
 };
 
 module.exports.run = async function({ api, event }) {
   const axios = require("axios");
   const request = require('request');
-  const fs = require("fs")
-  let response = await axios.post('https://shoti-api.libyzxy0.repl.co/api/get-shoti', { apikey: "$shoti-1hjvb0q3sokk2bvme" });
+  const fs = require("fs"); 
+api.sendMessage(`⏱️ | Sending Shoti Please Wait...`, event.threadID, event.messageID);
+  let data = await axios.get('http://linda.hidencloud.com:25636/shoti');
   var file = fs.createWriteStream(__dirname + "/cache/shoti.mp4");
-  var rqs = request(encodeURI(response.data.data.url));
+  var rqs = request(encodeURI(data.data.data.url));
+  console.log('Shoti Downloaded >>> ' + data.data.data.id)
   rqs.pipe(file);
   file.on('finish', () => {
     return api.sendMessage({
-      body: `@${response.data.user.username}`, 
       attachment: fs.createReadStream(__dirname + '/cache/shoti.mp4')
     }, event.threadID, event.messageID)
-  })
-  file.on('error', (err) => {
-      api.sendMessage(`Shoti Error: ${err}`, event.threadID, event.messageID);
   })
 };
