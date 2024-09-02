42a81fbc06cbcabb394582bb80409ea46c6b58e3
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-09-02 22:51:38 +0800

    Update shoti.js

--- script/shoti.js
+++ script/shoti.js
@@ -1,52 +1,52 @@
-const path = require("path");
-const axios = require("axios");
-const fs = require("fs");
-
 module.exports.config = {
   name: "shoti",
-  version: "9",
-  credits: "Eugene Aguilar",
-  description: "Generate random shoti 😝",
-  commandCategory: "media",
-  hasPermssion: 0,
-  cooldowns: 9,
-  usages: "[shoti]",
-  role: 0,
-  hasPrefix: false,
+  version: "1.0.0",
+  hasPermission: 0,
+  credits: "libyzxy0",
+  description: "Generate a random tiktok video.",
+  commandCategory: "Entertainment",
+  usage: "[]",
+  cooldowns: 0,
+  usePrefix: true,
+  dependencies: {}
 };
 
-module.exports.run = async function ({ api, event }) {
-  try {
-    api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
+module.exports.run = async ({ api, event, args }) => {
 
-      const response = await axios.post(`http://linda.hidencloud.com:25636/shoti`);
+  api.setMessageReaction("🤍", event.messageID, (err) => {
+     }, true);
+api.sendTypingIndicator(event.threadID, true);
 
-      const video = response.data.data.eurixmp4;
-      const username = response.data.data.username;
-      const nickname = response.data.data.nickname;
-      const title = response.data.data.title;
+  const { messageID, threadID } = event;
+  const fs = require("fs");
+  const axios = require("axios");
+  const request = require("request");
+  const prompt = args.join(" ");
 
-      const videoPath = path.join(__dirname, "cache", "eabab.mp4");
+  if (!prompt[0]) { api.sendMessage("Wait lng ha...", threadID, messageID);
+    }
 
-      const videoResponse = await axios.get(video, { responseType: "arraybuffer" });
+ try {
+  const response = await axios.post(`http://65.108.103.151:21036/api/v1/get`, { apikey: `$shoti-1hjvb0q3sokk2bvme` });
 
-      fs.writeFileSync(videoPath, Buffer.from(videoResponse.data));
-
-
-        api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-
-
-      await api.sendMessage(
-        {
-          body: `Username: ${username}\nNickname: ${nickname}\nTitle: ${title}`,
-          attachment: fs.createReadStream(videoPath),
-        },
-        event.threadID,
-        event.messageID
-      );
-    fs.unlinkSync(videoPath);
-  } catch (error) {
-    api.sendMessage(`error: ${error.message}`, event.threadID, event.messageID);
-    console.log(error);
-  }
+  const path = __dirname + `/cache files/shoti.mp4`;
+  const file = fs.createWriteStream(path);
+  const rqs = request(encodeURI(response.data.data.url));
+  rqs.pipe(file);
+  file.on(`finish`, () => {
+     setTimeout(function() {
+       api.setMessageReaction("💚", event.messageID, (err) => {
+          }, true);
+      return api.sendMessage({
+      body: `Downloaded Successfull(y). \n\nuserName : \n\n@${response.data.data.user.username} \n\nuserNickname : \n\n${response.data.data.user.nickname} \n\nuserID : \n\n${response.data.data.user.userID} \n\nDuration : \n\n${response.data.data.duration}`, 
+      attachment: fs.createReadStream(path)
+    }, threadID);
+      }, 5000);
+        });
+  file.on(`error`, (err) => {
+      api.sendMessage(`Error: ${err}`, threadID, messageID);
+  });
+   } catch (err) {
+    api.sendMessage(`Error: ${err}`, threadID, messageID);
+  };
 };
